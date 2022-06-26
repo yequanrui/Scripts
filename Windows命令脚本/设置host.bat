d2ab95e674e62a605b0eaac21a780d9405af8462
yequanrui <yequanrui@qq.com>
2022-06-26 21:57:07 +0800

    update

---
+++ Windows命令脚本/设置host.bat
@@ -0,0 +1,9 @@
+@echo off
+color 0F
+REM 声明采用UTF-8编码
+chcp 65001
+@attrib -r "%windir%\system32\drivers\etc\hosts"
+@echo # host start>>"%windir%\system32\drivers\etc\hosts"
+@echo 127.0.0.1 localhost>>"%windir%\system32\drivers\etc\hosts"
+@echo # host end >>"%windir%\system32\drivers\etc\hosts"
+@attrib +r "%windir%\system32\drivers\etc\hosts"
